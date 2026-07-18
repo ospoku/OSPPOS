2c8aeb1089d6401ec88bb75e67a2e20ac0407a3e
ospoku <ospoku@gmail.com>
2026-07-18 09:05:10 +0000

    fixing addinvoice view

--- OSPPOS/ViewComponents/AddInvoice.cs
+++ OSPPOS/ViewComponents/AddInvoice.cs
@@ -10,12 +10,19 @@ namespace OSPPOS.ViewComponents
     public class AddInvoice(XContext ctx):ViewComponent
     {
 
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-
+            var products = await ctx.Products
+              .Include(p => p.Category).Include(p=>p.Unit)
+              .Where(p => p.IsActive && p.CurrentStock > 0)
+              .OrderBy(p => p.Category.Name)
+              .ThenBy(p => p.Name)
+              .ToListAsync();
+            
             AddInvoiceVM addInvoiceVM = new()
             {
                 Items = [],
+                Products = products,
 
                 //Customers = [.. ctx.Customers.Select(c => new CustomerSelectList() { AllowCredit=c.AllowCredit,CreditLimit=c.CreditLimit,CustomerId=c.CustomerId })]
            Customers= new SelectList(ctx.Customers.Select(c=> new CustomerSelectList() {CustomerId=c.CustomerId,Name=c.Name }).ToList(),nameof(Customer.CustomerId),nameof(Customer.Name))
