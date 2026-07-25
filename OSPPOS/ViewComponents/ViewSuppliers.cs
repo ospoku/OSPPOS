52250d8efb6d10269185fcff92f9abdfb38ef492
ospoku <ospoku@gmail.com>
2026-07-25 09:24:34 +0000

    fixed settingstab

--- OSPPOS/ViewComponents/ViewSuppliers.cs
+++ OSPPOS/ViewComponents/ViewSuppliers.cs
@@ -4,7 +4,7 @@ using OSPPOS.ViewModels;
 
 namespace OSPPOS.ViewComponents
 {
-    public class ViewSuppliers(XContext ctx): Microsoft.AspNetCore.Mvc.ViewComponent
+    public class ViewSuppliers(XContext ctx): ViewComponent
     {
         public IViewComponentResult Invoke()
         { 
